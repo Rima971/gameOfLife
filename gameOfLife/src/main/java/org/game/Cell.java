bb295e07d381b7c9d928bdfed3ea1ebd8af797de
Rima971 <madhurima.poddar_ftc@external.swiggy.in>
2024-01-27 13:25:54 +0530

    test for cell creation exceptions on entering negative parameters

--- gameOfLife/src/main/java/org/game/Cell.java
+++ gameOfLife/src/main/java/org/game/Cell.java
@@ -1,14 +1,18 @@
-package org.example;
+package org.game;
+
+import java.security.InvalidParameterException;
 
 public class Cell {
     private int row, column;
     private boolean alive = false;
     public Cell(int row, int column){
+        if (row<0 || column<0) throw new InvalidParameterException("Cell coordinates cannot be negative");
         this.row = row;
         this.column = column;
     }
 
     public Cell(int row, int column, boolean alive){
+        if (row<0 || column<0) throw new InvalidParameterException("Cell coordinates cannot be negative");
         this.row = row;
         this.column = column;
         this.alive = alive;
--- gameOfLife/src/test/java/CellTest.java
+++ gameOfLife/src/test/java/CellTest.java
@@ -1,5 +1,9 @@
-import org.example.Cell;
+import org.game.Cell;
 import org.junit.Test;
+
+import java.security.InvalidParameterException;
+
+import static org.junit.Assert.assertThrows;
 
 public class CellTest {
     @Test()
@@ -7,4 +11,10 @@ public class CellTest {
         new Cell(2,3);
         new Cell(22, 12, true);
     }
+
+    @Test()
+    public void cell_creation_exceptions(){
+        assertThrows(InvalidParameterException.class, ()->new Cell(-2, -10));
+    }
+
 }
